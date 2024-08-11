443e12657f00b0776978a1f6e87aaa2b6abe02e6
Morti <mkoluzhnaya@mail.ru>
2024-08-11 20:57:42 +0300

    3

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pandas as pd
 
-
+#open and transform to dataframe
 file = pd.read_csv("games_dataset.csv")
 df = pd.DataFrame(file)
 
-
+#load all columns and rows
 pd.set_option("display.max_rows",None)
 
+
+#delete all rows, where value equals None
 df = df.dropna()
+
+#change colmn name 
 df.rename(columns={"Game Name":"Name"}, inplace=True)
 
+#filt year
 df = df[df['Release Year'] > 2014]
-df_group = df.groupby("Name")
 
-print(df_group.head(5))
+df.sort_values(by='Name',inplace=True)
 
-#lllllll
+print(df)
 
-
+#print(df['User Rating'].mean())
+# print(df.groupby(["Platform"]).mean())
+# print(df['Platform'])
